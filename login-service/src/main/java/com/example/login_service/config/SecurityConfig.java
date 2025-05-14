cb4a1dcc0c9ce4b63ffd0d10a9201bebcaba807c
Masterx43 <bastiancito.26@gmail.com>
2025-05-14 18:15:01 -0400

    Added Service to reservaServices

--- login-service/src/main/java/com/example/login_service/config/SecurityConfig.java
+++ login-service/src/main/java/com/example/login_service/config/SecurityConfig.java
@@ -5,10 +5,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
 
--- reserva-service/src/main/java/com/example/reserva_service/service/ReservaService.java
+++ reserva-service/src/main/java/com/example/reserva_service/service/ReservaService.java
@@ -1,5 +1,54 @@
 package com.example.reserva_service.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.reserva_service.model.Reserva;
+import com.example.reserva_service.model.Usuario;
+import com.example.reserva_service.repository.ReservaRepository;
+import com.example.reserva_service.repository.ServicioRepository;
+import com.example.reserva_service.repository.UsuarioRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class ReservaService {
+    @Autowired
+    private ReservaRepository reservaRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ServicioRepository servicioRepository;
+
+    public List<Reserva> listarReservas() {
+        return reservaRepository.findAll();
+    }
+
+    public Reserva buscarPorId(Long id){
+        return reservaRepository.findById(id).orElseThrow();
+    }
+
+    public Reserva agregarReserva(Reserva reserva, Long idUsuario, Long idSercivio){
+        if (usuarioRepository.existsById(idUsuario) == false){
+            return null;
+        }
+        if (servicioRepository.existsById(idSercivio)){
+            return null;
+        }
+        return reservaRepository.save(reserva);
+    }
+
+    public void borrarReserva(Long id) {
+        reservaRepository.deleteById(id);
+    }
+
+    public List<Reserva> listarPorIdUser(Usuario usuario){
+        return reservaRepository.findByUsuario(usuario);
+    }
 
 }
