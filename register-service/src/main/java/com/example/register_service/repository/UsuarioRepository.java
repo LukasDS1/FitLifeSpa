09abcc100d641d5bfad7c3d67b0e2629bca492be
Masterx43 <bastiancito.26@gmail.com>
2025-05-21 17:09:49 -0400

    changes to Register-service

--- register-service/src/main/java/com/example/register_service/repository/UsuarioRepository.java
+++ register-service/src/main/java/com/example/register_service/repository/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package com.example.register_service.repository;
 
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +7,5 @@ import com.example.register_service.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
-    Optional<Usuario> findByEmail(String email);
+    Usuario findByEmail(String email);
 }
