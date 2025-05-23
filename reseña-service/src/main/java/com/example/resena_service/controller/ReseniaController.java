5a6e4421e6f26b216b48f6613ddfafebdd1404ee
Masterx43 <bastiancito.26@gmail.com>
2025-05-22 21:02:20 -0400

    Added resenia-service

---
+++ reseña-service/src/main/java/com/example/resena_service/controller/ReseniaController.java
@@ -0,0 +1,67 @@
+package com.example.resena_service.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.resena_service.model.Resenia;
+import com.example.resena_service.service.ReseniaService;
+
+@RestController
+@RequestMapping("/api-v1/resenias")
+public class ReseniaController {
+    @Autowired
+    private ReseniaService reseniaService;
+
+    @GetMapping("/total")
+    public ResponseEntity<List<Resenia>> findAllResenias(){
+        List<Resenia> resenias = reseniaService.listar();
+        if (resenias.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(resenias);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id){
+        try {
+            Resenia resenia = reseniaService.buscarId(id);
+            return ResponseEntity.ok(resenia);
+        } catch (Exception e) {
+            // TODO: handle exception
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> addResenia(@RequestBody Resenia resenia){
+        Resenia resenia1 = reseniaService.agregarResenia(resenia);
+        if (resenia1 == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Faltan campos o esta mal estructurado el body");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(resenia1);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteResenia(Long id){
+        Resenia resenias = reseniaService.buscarId(id);
+        if (resenias == null) {
+            return ResponseEntity.notFound().build();
+        } else{
+            reseniaService.Eliminar(id);
+            return ResponseEntity.ok("El contenido se elimino correctamente");
+        }
+
+    }
+
+}
