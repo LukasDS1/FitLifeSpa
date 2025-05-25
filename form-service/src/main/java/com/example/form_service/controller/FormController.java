835ed3b28e0569a307e581ee4964ae40cb849367
Benjamin Moretti <be.moretti@duocuc.cl>
2025-05-24 23:09:22 -0400

    Added get method to controller
    form controller get method added (get by Id)

--- form-service/src/main/java/com/example/form_service/controller/FormController.java
+++ form-service/src/main/java/com/example/form_service/controller/FormController.java
@@ -12,25 +12,39 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
-@RequestMapping("/api-v1")
+@RequestMapping("/api-v1/form")
 @RequiredArgsConstructor
 public class FormController {
 
     private final FormService formService;
 
-    @PostMapping("/form")
-    public ResponseEntity<?> saveForm(@RequestBody Form form) {
+    @PostMapping("/post")
+    public ResponseEntity<String> saveForm(@RequestBody Form form) {
         try {
             if (formService.saveForm(form)) {
-                return ResponseEntity.status(HttpStatus.CREATED).body("formulario creado:\n" + form);
+                return ResponseEntity.status(HttpStatus.CREATED).body("formulario creado con éxito.");
             }
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al crear el formulario");
         } catch (Exception e) {
             throw new RuntimeException("Ha ocurrido una excepción al crear el formulario:" + e.getMessage());
         }
     }
+
+    @GetMapping("/get/{idForm}")
+    public ResponseEntity<Form> getFormById(@PathVariable Long idForm) {
+        try {
+            if (idForm != null) {
+                return ResponseEntity.status(HttpStatus.OK).body(formService.getForm(idForm));
+            }
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            throw new RuntimeException("Error al buscar formulario por id: " + e.getStackTrace());
+        }
+    }
+    
     
 }
--- form-service/src/main/java/com/example/form_service/service/FormService.java
+++ form-service/src/main/java/com/example/form_service/service/FormService.java
@@ -13,11 +13,29 @@ public class FormService {
 
     private final FormRepository formRepository;
 
+    /**
+     * 
+     * @param form objecto de formulario
+     * @return si guarda el formulario {@code true}, de otra forma {@code false}.
+     */
     public Boolean saveForm(Form form) {
         if(form != null) {
             formRepository.save(form);
             return true;
         }
         return false;
+    }
+
+    /**
+     * 
+     * @param idForm como id del formulario
+     * @return si encuentra un formulario devuelve un objeto {@code Form}
+     * @throws RuntimeException si no existe el formulario.
+     */
+    public Form getForm(Long idForm) {
+        if (formRepository.existsById(idForm)) {
+            return formRepository.findById(idForm).get();
+        }
+        throw new RuntimeException("No existe el formulario de id: " + idForm);
     }
 }
